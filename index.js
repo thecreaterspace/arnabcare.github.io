a30eddc509fbb72434c69d696011f77da3e36e83
thecreaterspace <31860189+thecreaterspace@users.noreply.github.com>
2017-09-12 12:12:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+noStroke();
 alert("Welcome to ArnabCare!");
+fill(20, 20, 20,);
+triangle(200, 200, 200, 200,);
+fill(60, 60, 60,);
+ellipse(400, 400, 400, 400,);
+fill(40, 40, 40,);
+rect(100, 100, 100, 100,);
